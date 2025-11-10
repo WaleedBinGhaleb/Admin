130893b68b10f871dc17af7c5b25df6a34edc336
Wizza <34290001+WaleedBinGhaleb@users.noreply.github.com>
2025-11-11 00:11:55 +0200

    1.3.2

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -51,7 +51,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "976f910fc4c8c4e1cd6ba9966e56144f
 "assets/images/Requests.png": "51acef050ac501e2c3cad1491807c567",
 "assets/images/search.png": "548e1cdbd34c902e94ca10be6fc151e4",
 "assets/images/sign%2520contract.png": "4b1431584131eaf11f97cf338d0e74a0",
-"assets/NOTICES": "43aad869c48a4a19e1d0ca4cb04b7d4e",
+"assets/NOTICES": "ef6bdde00a132b1f7ba21edb66548f03",
 "assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "33b7d9392238c04c131b6ce224e13711",
 "assets/packages/font_awesome_flutter/lib/fonts/fa-brands-400.ttf": "4769f3245a24c1fa9965f113ea85ec2a",
 "assets/packages/font_awesome_flutter/lib/fonts/fa-regular-400.ttf": "94f8b312818e013126aaf7e48ca91b13",
@@ -80,16 +80,16 @@ const RESOURCES = {"assets/AssetManifest.bin": "976f910fc4c8c4e1cd6ba9966e56144f
 "canvaskit/skwasm.wasm": "39dd80367a4e71582d234948adc521c0",
 "favicon.png": "7fd4d5e3f746b180be3786f2769bb56b",
 "flutter.js": "83d881c1dbb6d6bcd6b42e274605b69c",
-"flutter_bootstrap.js": "ab6044ccf6948ac0e9b6a665adac1337",
+"flutter_bootstrap.js": "1c212622ea344acb29c966b790349fb9",
 "icons/Icon-192.png": "aa525253c529696b2ee57ba5208231ad",
 "icons/Icon-512.png": "cf5b3097b78b2655f70609507fa54ddf",
 "icons/Icon-maskable-192.png": "09ebf98ba8c493e9b2f4234d2fe7ae3a",
 "icons/Icon-maskable-512.png": "34149ebc0e866d8711427a858db0b56c",
 "index.html": "ab0ac05d94bc91e446e235bbf37d1c6f",
 "/": "ab0ac05d94bc91e446e235bbf37d1c6f",
-"main.dart.js": "66b9f76d69294240746a1e7c0bb4052e",
+"main.dart.js": "51875a4160bac1ee399303b58220e810",
 "manifest.json": "02655fbc5b4f12522b12e8dcb7613fbf",
-"version.json": "5d9bf924ad719ec1d8543020b6857f48"};
+"version.json": "8eb712952e486b64aafeacbe0599632e"};
 // The application shell files that are downloaded before a service worker can
 // start.
 const CORE = ["main.dart.js",
